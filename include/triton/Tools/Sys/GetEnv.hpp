4012d53f7ebf59ec4b3dfd9b32ba2da8f0b904cd
Shucai Xiao <shucai.xiao@amd.com>
2024-04-16 16:23:49 -0500

    add truncate of fp32 to bf16

--- include/triton/Tools/Sys/GetEnv.hpp
+++ include/triton/Tools/Sys/GetEnv.hpp
@@ -43,6 +43,7 @@ inline const std::set<std::string> ENV_VARS = {
     "MLIR_ENABLE_DIAGNOSTICS",
     "TRITON_ENABLE_LLVM_DEBUG",
     "USE_TTGIR_LOC",
+    "TRITON_TRUNCATE_F32_TO_BF16",
 };
 
 namespace tools {
