f9788644cdaeca9b10e727901bc7563df6c0f5a3
Alexander Efimov <efimov.alexander@gmail.com>
2024-07-11 13:10:43 +0000

    support dot3d

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -68,3 +68,5 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
                   mlir::NVVM::NVVMDialect, mlir::triton::nvgpu::NVGPUDialect,
                   mlir::ROCDL::ROCDLDialect>();
 }
+
+control flow dialect + math dialct + arith dialect + scf
